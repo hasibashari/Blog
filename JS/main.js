a6e39616e8229164edf9d7fa6cd303aa53999e3c
hasibashari <hasibashari@gmail.com>
2022-11-05 22:18:11 +0700

    Menambahkan blog post dan style

--- JS/main.js
+++ JS/main.js
@@ -6,4 +6,7 @@ $(document).ready(function () {
   $toggleCollapse.click(function () {
     $nav.toggleClass("collapse");
   });
+
+  /* owl-crousel for blog */
+  $(".owl-carousel").owlCarousel();
 });
